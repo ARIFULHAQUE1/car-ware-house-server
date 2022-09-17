c9cc413e5c947d976931282c6c35f194646d55ff
ARIFULHAQUE1 <arifulhaquearif2326@gmail.com>
2022-09-17 13:59:00 +0600

    create update quantity api

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 require('dotenv').config();
-
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 app.use(cors());
@@ -10,41 +10,72 @@ app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4j9kaod.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-
-  // perform actions on the collection object
-  client.close();
-});
-
-
-
 
 async function run() {
-  try{
-   await client.connect();
-  //  all inventorys item
-   const inventoryCollection = client.db("carhouse").collection("inventorys");
-    app.get('/inventory',async (req,res) =>{
+  try {
+    await client.connect();
+    //  all inventorys item
+    const inventoryCollection = client.db("carhouse").collection("inventorys");
+    app.get('/inventory', async (req, res) => {
       const query = {};
       const cursor = inventoryCollection.find(query);
       const inventory = await cursor.toArray();
       res.send(inventory);
     })
+// load single item from database 
+    app.get('/inventory/:id', async (req, res) => {
+      const id = req.params.id;
+     
+      const query = {_id: ObjectId(id) };
+      const result = await inventoryCollection.findOne(query);
+      res.send(result);
+    })
+
+//  handle quantity and delivered quantity
+
+    app.put('/handleQuantity/:id', async (req,res)=>{
+      const id = req.params.id;
+      const updateQuantity = req.body;
+     
+      const filter = {_id: ObjectId(id)};
+      const options = { upsert: true };
+        const updateDoc = {$set : updateQuantity}
+        const result = await inventoryCollection.updateOne(filter,  updateDoc,options)
+      res.send(result)
+    })
+
+// update qunatity
+    app.put('/updateQuantity/:id', async (req,res)=>{
+      const id = req.params.id;
+      const updateQuantity = req.body;
+   
+      const filter = {_id: ObjectId(id)};
+      const options = { upsert: true };
+        const updateDoc = {$set : updateQuantity}
+        const result = await inventoryCollection.updateOne(filter,  updateDoc,options)
+      res.send(result)
+    })
+
+    // Delete item from database
+    app.delete('/inventory/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = {_id: ObjectId(id) };
+      const result = await inventoryCollection.deleteOne(query);
+      res.send(result);
+    })
 
   }
-  finally{
+  finally {
 
   }
 
 } run().catch(console.dir)
 
 
-app.get('/', (req, res) => {
-  res.send('Hello ware house !')
-})
+
 
 app.listen(port, () => {
   console.log(` listening on ware-house-car ${port}`)
